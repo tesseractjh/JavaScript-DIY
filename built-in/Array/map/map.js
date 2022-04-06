3fa5c782dc462a66778dca374be4684f756ce76a
Tesseract <tensai3355@gmail.com>
2022-04-06 10:42:22 +0900

    :bug: 테스트 코드를 통과하도록 수정

--- built-in/Array/map/map.js
+++ built-in/Array/map/map.js
@@ -1,12 +1,12 @@
 const myMap = function (callback, thisArg) {
   const array = [];
-  const iter = Object.entries(this);
-  for (const [key, value] of iter) {
-    array[key] = callback.bind(thisArg)(value, Number(key), this);
+  for (const key in this) {
+    const intKey = Number(key);
+    if (Number.isInteger(intKey)) {
+      array[key] = callback.bind(thisArg)(this[key], intKey, this);
+    }
   }
   return array;
 };
-
-Array.prototype.myMap = myMap;
 
 module.exports = myMap;
